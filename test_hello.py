36b0fa5d765b72c936166c12a28cb82fabed646e
EC2 Default User <ec2-user@ip-172-31-9-251.ec2.internal>
2022-10-23 21:07:20 +0000

    initial commit

---
+++ test_hello.py
@@ -0,0 +1,20 @@
+from hello import toyou, add, subtract
+
+
+def setup_function(function):
+    print("Running Setup: %s" % {function.__name__})
+    function.x = 10
+
+
+def teardown_function(function):
+    print("Running Teardown: %s" % {function.__name__})
+    del function.x
+
+
+### Run to see failed test
+# def test_hello_add():
+# assert subtract(test_hello_subtract.x) == 12
+
+
+def test_hello_subtract():
+    assert subtract(test_hello_subtract.x) == 9
